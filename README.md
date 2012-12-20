5dba28fb589ff8770edfcd51be949f415ab0eb18
Chris Dickinson <christopher.s.dickinson@gmail.com>
2012-12-19 21:20:46 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# rewrite-js
+
+A CLI tool for transforming JavaScript input using [falafel](http://github.com/substack/node-falafel)
+and [cssauron](http://github.com/chrisdickinson/cssauron). Works great with Vim!
+
+```bash
+
+# usage: rewrite-ms [tranform-module, ...]
+$ cat myfile.js | rewrite-js transform.js > myfile-transformed.js
+
+```
+
+Transformation modules should export and object mapping [cssauron-falafel](http://github.com/chrisdickinson/cssauron-falafel)
+selectors to transformation functions.
+
+```javascript
+// example transform
+module.exports = {
+  'call:contains(async) > function:last-child': rewrite_async_wrap
+}
+
+function rewrite_async_wrap(node) {
+  node.parent.update(node.source())
+}
+
+// takes `async(function() { })` and rewrites it to `function() { }`
+```
+
+# License
+
+MIT
---
+++ index.js
@@ -0,0 +1,78 @@
+var tty = require('tty')
+  , path = require('path')
+  , fs = require('fs')
+
+var concat = require('concat-stream')
+  , nopt = require('nopt')
+  , language = require('cssauron-falafel')
+  , falafel = require('falafel')
+  , shorthand
+  , options
+
+options = {
+  'help': Boolean
+}
+
+shorthand = {
+  'h': ['--help']
+}
+
+module.exports = run
+
+function help() {
+/*
+rewrite-js [transform-module ...]
+
+  Takes a JavaScript program as input on stdin, outputs the program transformed
+  by each of the transform-modules on stdout.
+*/
+
+  var str = help+''
+
+  process.stdout.write(str.slice(str.indexOf('/*')+3, str.indexOf('*/')))
+}
+
+function run() {
+  var parsed = nopt(options, shorthand)
+    , stdintty = tty.isatty(process.stdin)
+    , transform = null
+    , source = null
+
+  if(parsed.help || (!parsed.argv.remain.length && stdintty)) {
+    return help(), process.exit(1)
+  }
+
+  process.stdin.pipe(concat(got_source))
+  process.stdin.resume()
+
+  function got_source(err, data) {
+    if(err) throw err
+
+    source = data
+    transform = parse_transform(require(path.join(process.cwd(), parsed.argv.remain[0])))
+    source = falafel(source+'', apply_transform)
+
+    if(!parsed.argv.remain.length) {
+      return process.stdout.write(source)
+    }
+
+    parsed.argv.remain.shift()
+    got_source(null, source)
+  }
+
+  function apply_transform(node) {
+    for(var i = 0, len = transform.length; i < len; ++i) {
+      if(transform[i][0](node)) {
+        if(transform[i][1](node) === false) break
+      }
+    } 
+  }
+
+  function parse_transform(trans) {
+    var output = []
+    for(var key in trans) {
+      output.push([language(key), typeof trans[key] === 'string' ? Function('node', 'return '+trans[key]) : trans[key]])
+    }
+    return output
+  }
+}
